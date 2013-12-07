0ce03c785264973d6353c133c5eb524cc0193578
theDazzler <dguin7@yahoo.com>
2013-12-07 16:29:19 -0500

    deleted build.old folder

--- AndroidBootstrap/build.old/source/buildConfig/debug/com/beardedhen/androidbootstrap/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.beardedhen.androidbootstrap;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
--- AndroidBootstrapTest/build.old/source/buildConfig/debug/com/beardedhen/androidbootstraptest/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.beardedhen.androidbootstraptest;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
