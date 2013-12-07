38b75fe4fa96ad59926ac525871c39247da4e579
theDazzler <dguin7@yahoo.com>
2013-12-07 03:08:16 -0500

    added heading

---
+++ AndroidBootstrap/build.old/source/buildConfig/release/com/beardedhen/androidbootstrap/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.beardedhen.androidbootstrap;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+}
---
+++ AndroidBootstrapTest/build.old/source/buildConfig/debug/com/beardedhen/androidbootstraptest/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.beardedhen.androidbootstraptest;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
