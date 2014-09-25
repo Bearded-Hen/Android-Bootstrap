9d8f184beda10ca16cb90a2d299f1c4acc5c4683
qwerw2 <er450203@gmail.com>
2014-09-25 20:24:19 +0800

    Fix #55 It caused NumerError by indicating button's size
    It will happen NumberFormatException when indicating button's layout_width
    ex. java.lang.NumberFormatException: Invalid float: "50.0dip"

--- AndroidBootstrap/src/com/beardedhen/androidbootstrap/BootstrapButton.java
+++ AndroidBootstrap/src/com/beardedhen/androidbootstrap/BootstrapButton.java
@@ -172,7 +172,7 @@ public class BootstrapButton extends FrameLayout {
 		
 		int layoutWidth = 0;
 		if(a.getString(R.styleable.BootstrapButton_android_layout_width) != null) {
-			layoutWidth = a.getInt(R.styleable.BootstrapButton_android_layout_width, 0);
+			layoutWidth = (int) a.getDimension(R.styleable.BootstrapButton_android_layout_width, 0);
 		}
 		
 		//works even if it's fill_parent or match_parent 
