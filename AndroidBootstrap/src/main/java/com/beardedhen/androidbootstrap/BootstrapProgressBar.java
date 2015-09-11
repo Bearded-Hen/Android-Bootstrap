8eff66931f78e76e4d5831f7bcca9d31a618118e
Jamie <fractalwrench@gmail.com>
2015-09-11 21:06:24 +0100

    draw stripey bar & cache bitmap

--- AndroidBootstrap/src/main/java/com/beardedhen/androidbootstrap/BootstrapProgressBar.java
+++ AndroidBootstrap/src/main/java/com/beardedhen/androidbootstrap/BootstrapProgressBar.java
@@ -2,11 +2,12 @@ package com.beardedhen.androidbootstrap;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 
@@ -19,6 +20,7 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
     private static final long ANIMATOR_PROGRESS_MS = 500;
 
     private Paint progressPaint;
+    private Paint stripePaint;
     private Paint bgPaint;
 
     private int userProgress;
@@ -28,6 +30,7 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
     private boolean animated;
 
     private ValueAnimator progressAnimator;
+    private Bitmap stripeTile;
 
     public BootstrapProgressBar(Context context) {
         super(context);
@@ -48,10 +51,17 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
         this.userProgress = 0;
         this.drawnProgress = 0;
 
+        int b = Color.BLUE;
 
         progressPaint = new Paint();
         progressPaint.setStyle(Paint.Style.FILL);
-        progressPaint.setColor(Color.BLUE);
+        progressPaint.setColor(Color.argb(255, Color.red(b), Color.green(b), Color.blue(b)));
+        progressPaint.setAntiAlias(true);
+
+        stripePaint = new Paint();
+        stripePaint.setStyle(Paint.Style.FILL);
+        stripePaint.setColor(Color.argb(150, Color.red(b), Color.green(b), Color.blue(b)));
+        stripePaint.setAntiAlias(true);
 
         bgPaint = new Paint();
         bgPaint.setStyle(Paint.Style.FILL);
@@ -69,7 +79,7 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
         this.drawnProgress = userProgress; // previously set value
         this.userProgress = progress;
 
-        this.animated = true;
+        this.animated = true; // FIXME
 
         if (animated) {
             clearAnimation();
@@ -80,7 +90,6 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
             progressAnimator.setInterpolator(new DecelerateInterpolator());
             progressAnimator.addUpdateListener(this);
             progressAnimator.start();
-
         }
         else {
             this.drawnProgress = progress;
@@ -112,8 +121,6 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
 
     @Override public void onAnimationUpdate(ValueAnimator animation) {
         this.drawnProgress = (int) ((float) animation.getAnimatedValue());
-
-        Log.d("Bootstrap", String.format("Draw update %d", drawnProgress));
         invalidate();
     }
 
@@ -140,6 +147,14 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
         setMeasuredDimension(width, height);
     }
 
+    @Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (h != oldh) {
+            stripeTile = null; // dereference cached bitmap
+        }
+
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
     @Override protected void onDraw(Canvas canvas) {
         float w = getWidth();
         float h = getHeight();
@@ -147,8 +162,58 @@ public class BootstrapProgressBar extends View implements ProgressView, ValueAni
         float ratio = (float) (drawnProgress / 100.0);
         int lineEnd = (int) (w * ratio);
 
-        canvas.drawRect(0, 0, lineEnd, h, progressPaint);
+        this.striped = true;// FIXME
+
+        // TODO animated stripes should have an infinite ValueAnimator, offset the bitmap position here
+
+        if (striped) {
+
+            if (stripeTile == null) {
+                stripeTile = createTile(h);
+            }
+
+            Paint paint = new Paint();
+            float start = 0;
+
+            while (start < lineEnd) {
+                canvas.drawBitmap(stripeTile, start, 0, paint);
+                start += stripeTile.getHeight() * 2;
+            }
+        }
+        else {
+            canvas.drawRect(0, 0, lineEnd, h, progressPaint);
+        }
         canvas.drawRect(lineEnd, 0, w, h, bgPaint);
     }
+
+    private Bitmap createTile(float h) {
+        Bitmap bm = Bitmap.createBitmap((int) h * 2, (int) h, Bitmap.Config.ARGB_8888);
+        Canvas tile = new Canvas(bm);
+
+        float x = 0;
+        Path path = new Path();
+
+        path.moveTo(x, 0);
+        path.lineTo(x, h);
+        path.lineTo(h, h);
+        tile.drawPath(path, stripePaint); // draw striped triangle
+
+        path.reset();
+        path.moveTo(x, 0);
+        path.lineTo(x + h, h);
+        path.lineTo(x + (h * 2), h);
+        path.lineTo(x + h, 0);
+        tile.drawPath(path, progressPaint); // draw progress parallelogram
+
+        x += h;
+        path.reset();
+        path.moveTo(x, 0);
+        path.lineTo(x + h, 0);
+        path.lineTo(x + h, h);
+        tile.drawPath(path, stripePaint); // draw striped triangle (completing tile)
+
+        return bm;
+    }
+
 
 }
