36ecf9627492c3fd1bc249c9ac52ed0bd4677aeb
Jamie <fractalwrench@gmail.com>
2015-09-20 14:35:09 +0100

    update button group examples

--- TODO.md
+++ TODO.md
@@ -1,16 +1,16 @@
 Fix List
 =============
 
-* Improve BootstrapButtonGroup examples/test cases
 * Make BootstrapProgressBar roundable
-* Bootstrap Sizing values (H1-H6, button)
+* Bootstrap Sizing values as ratios (H1-H6, button)
 * Custom Button styles for Secondary/Link themes (create specific methods for each)
 
-Feature List
+Feature (Wish)List
 =============
 
 * Support another font by default
 * Add BootstrapCardView and BootstrapCardGroupView to replace BootstrapThumbnail
 * Add Bootstrap input groups to replace BootstrapEditText
 * Bootstrap layout system
-
+* Generate gh-pages
+* Write short tutorial
--- sample/src/main/java/com/example/sample/BootstrapButtonGroupExample.java
+++ sample/src/main/java/com/example/sample/BootstrapButtonGroupExample.java
@@ -1,6 +1,15 @@
 package com.example.sample;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapButtonGroup;
+import com.beardedhen.androidbootstrap.api.defaults.DefaultBootstrapBrand;
+import com.beardedhen.androidbootstrap.api.defaults.DefaultBootstrapSize;
+
+import butterknife.Bind;
+import butterknife.OnClick;
 
 public class BootstrapButtonGroupExample extends BaseActivity {
 
@@ -10,6 +19,82 @@ public class BootstrapButtonGroupExample extends BaseActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Bind(R.id.bbutton_group_orientation_change) BootstrapButtonGroup orientationChange;
+    @Bind(R.id.bbutton_group_size_change) BootstrapButtonGroup sizeChange;
+    @Bind(R.id.bbutton_group_outline_change) BootstrapButtonGroup outlineChange;
+    @Bind(R.id.bbutton_group_rounded_change) BootstrapButtonGroup roundedChange;
+    @Bind(R.id.bbutton_group_brand_change) BootstrapButtonGroup brandChange;
+    @Bind(R.id.bbutton_group_child_change) BootstrapButtonGroup childChange;
+
+    @OnClick(R.id.bbutton_group_orientation_change_btn) void onOrientationChangeExampleClicked() {
+        boolean isHorizontal = orientationChange.getOrientation() == LinearLayout.HORIZONTAL;
+        int newOrientation = isHorizontal ? LinearLayout.VERTICAL : LinearLayout.HORIZONTAL;
+        orientationChange.setOrientation(newOrientation);
+    }
+
+    @OnClick(R.id.bbutton_group_outline_change_btn) void onOutlineChangeExampleClicked() {
+        outlineChange.setShowOutline(!outlineChange.isShowOutline());
+    }
+
+    @OnClick(R.id.bbutton_group_rounded_change_btn) void onRoundedChangeExampleClicked() {
+        roundedChange.setRounded(!roundedChange.isRounded());
+    }
+
+    @OnClick(R.id.bbutton_group_child_add_btn) void onChildAddExampleClicked() {
+        int count = childChange.getChildCount();
+
+        BootstrapButton button = new BootstrapButton(this);
+        button.setText(String.format("%d", count + 1));
+
+        childChange.addView(button);
+    }
+
+    @OnClick(R.id.bbutton_group_child_remove_btn) void onChildRemoveExampleClicked() {
+        int count = childChange.getChildCount();
+
+        if (count > 0) {
+            childChange.removeViewAt(count - 1);
+        }
+    }
+
+    @OnClick(R.id.bbutton_group_brand_change_btn) void onBrandChangeExampleClicked() {
+        switch ((DefaultBootstrapBrand) brandChange.getBootstrapBrand()) {
+
+            case PRIMARY:
+                brandChange.setBootstrapBrand(DefaultBootstrapBrand.SUCCESS);
+                break;
+            case SUCCESS:
+                brandChange.setBootstrapBrand(DefaultBootstrapBrand.INFO);
+                break;
+            case INFO:
+                brandChange.setBootstrapBrand(DefaultBootstrapBrand.WARNING);
+                break;
+            case WARNING:
+                brandChange.setBootstrapBrand(DefaultBootstrapBrand.DANGER);
+                break;
+            case DANGER:
+                brandChange.setBootstrapBrand(DefaultBootstrapBrand.REGULAR);
+                break;
+            case REGULAR:
+                brandChange.setBootstrapBrand(DefaultBootstrapBrand.PRIMARY);
+                break;
+        }
+    }
+
+    @OnClick(R.id.bbutton_group_size_change_btn) void onSizeChangeExampleClicked() {
+        switch ((DefaultBootstrapSize) sizeChange.getBootstrapSize()) {
+            case MEDIUM:
+                sizeChange.setBootstrapSize(DefaultBootstrapSize.SMALL);
+                break;
+            case SMALL:
+                sizeChange.setBootstrapSize(DefaultBootstrapSize.LARGE);
+                break;
+            case LARGE:
+                sizeChange.setBootstrapSize(DefaultBootstrapSize.MEDIUM);
+                break;
+        }
     }
 
 }
