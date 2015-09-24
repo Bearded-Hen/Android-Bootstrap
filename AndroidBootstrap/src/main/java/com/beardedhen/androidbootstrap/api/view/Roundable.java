30932cfd02c97d9a1b41a7162b63a09493126acc
Jamie <fractalwrench@gmail.com>
2015-09-24 19:07:20 +0100

    update todo

---
+++ AndroidBootstrap/src/main/java/com/beardedhen/androidbootstrap/api/view/Roundable.java
@@ -0,0 +1,11 @@
+package com.beardedhen.androidbootstrap.api.view;
+
+public interface Roundable {
+
+    String KEY = "com.beardedhen.androidbootstrap.api.view.Roundable";
+
+    void setRoundedCorners(boolean roundable);
+
+    boolean isRoundedCorners();
+
+}
