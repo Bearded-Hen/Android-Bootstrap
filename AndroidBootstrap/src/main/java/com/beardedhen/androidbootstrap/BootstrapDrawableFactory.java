6575e39e3d44a22cbf801f8e2460f2ffe5c5a1a7
Jamie <fractalwrench@gmail.com>
2015-10-07 19:43:39 +0100

    final tidy up

--- AndroidBootstrap/src/main/java/com/beardedhen/androidbootstrap/BootstrapDrawableFactory.java
+++ AndroidBootstrap/src/main/java/com/beardedhen/androidbootstrap/BootstrapDrawableFactory.java
@@ -18,9 +18,22 @@ import com.beardedhen.androidbootstrap.utils.DimenUtils;
 
 /**
  * Provides a factory for generating Drawables which are used as the backgrounds for Bootstrap Views.
+ * This avoids needing to define hundreds of XML resource files.
  */
 class BootstrapDrawableFactory {
 
+    /**
+     * Generates a background drawable for a Bootstrap Button
+     *
+     * @param context      the current context
+     * @param brand        the bootstrap brand
+     * @param strokeWidth  the stroke width in px
+     * @param cornerRadius the corner radius in px
+     * @param position     the position of the button in its parent view
+     * @param showOutline  whether the button should be outlined
+     * @param rounded      whether the corners should be rounded
+     * @return a background drawable for the BootstrapButton
+     */
     static Drawable bootstrapButton(Context context,
                                     BootstrapBrand brand,
                                     int strokeWidth,
@@ -33,16 +46,9 @@ class BootstrapDrawableFactory {
         GradientDrawable activeGd = new GradientDrawable();
         GradientDrawable disabledGd = new GradientDrawable();
 
-        if (showOutline) {
-            defaultGd.setColor(Color.TRANSPARENT);
-            activeGd.setColor(brand.activeFill(context));
-            disabledGd.setColor(Color.TRANSPARENT);
-        }
-        else {
-            defaultGd.setColor(brand.defaultFill(context));
-            activeGd.setColor(brand.activeFill(context));
-            disabledGd.setColor(brand.disabledFill(context));
-        }
+        defaultGd.setColor(showOutline ? Color.TRANSPARENT : brand.defaultFill(context));
+        activeGd.setColor(showOutline ? brand.activeFill(context) : brand.activeFill(context));
+        disabledGd.setColor(showOutline ? Color.TRANSPARENT : brand.disabledFill(context));
 
         defaultGd.setStroke(strokeWidth, brand.defaultEdge(context));
         activeGd.setStroke(strokeWidth, brand.activeEdge(context));
@@ -64,25 +70,8 @@ class BootstrapDrawableFactory {
         return setupStateDrawable(position, strokeWidth, defaultGd, activeGd, disabledGd);
     }
 
-    static ColorStateList bootstrapButtonText(Context context, boolean outline, BootstrapBrand brand) {
-
-        int defaultColor = outline ? brand.defaultFill(context) : brand.defaultTextColor(context);
-        int activeColor = outline ? ColorUtils.resolveColor(android.R.color.white, context) : brand.activeTextColor(context);
-        int disabledColor = outline ? brand.disabledFill(context) : brand.disabledTextColor(context);
-
-        if (outline && brand instanceof DefaultBootstrapBrand) { // special case
-            DefaultBootstrapBrand db = (DefaultBootstrapBrand) brand;
-
-            if (db == DefaultBootstrapBrand.SECONDARY) {
-                defaultColor = context.getResources().getColor(R.color.bootstrap_brand_secondary_border);
-                disabledColor = defaultColor;
-            }
-        }
-        return new ColorStateList(getStateList(), getColorList(defaultColor, activeColor, disabledColor));
-    }
-
     /**
-     * Generates a Drawable for a Bootstrap Label background
+     * Generates a Drawable for a Bootstrap Label background, according to state parameters
      *
      * @param context        the current context
      * @param bootstrapBrand the BootstrapBrand theming whose colors should be used
@@ -182,6 +171,113 @@ class BootstrapDrawableFactory {
         return drawable;
     }
 
+    /**
+     * Generates a colorstatelist for a bootstrap button
+     *
+     * @param context the current context
+     * @param outline whether the button is outlined
+     * @param brand   the button brand
+     * @return the color state list
+     */
+    static ColorStateList bootstrapButtonText(Context context, boolean outline, BootstrapBrand brand) {
+
+        int defaultColor = outline ? brand.defaultFill(context) : brand.defaultTextColor(context);
+        int activeColor = outline ? ColorUtils.resolveColor(android.R.color.white, context) : brand.activeTextColor(context);
+        int disabledColor = outline ? brand.disabledFill(context) : brand.disabledTextColor(context);
+
+        if (outline && brand instanceof DefaultBootstrapBrand) { // special case
+            DefaultBootstrapBrand db = (DefaultBootstrapBrand) brand;
+
+            if (db == DefaultBootstrapBrand.SECONDARY) {
+                defaultColor = ColorUtils.resolveColor(R.color.bootstrap_brand_secondary_border, context);
+                disabledColor = defaultColor;
+            }
+        }
+        return new ColorStateList(getStateList(), getColorList(defaultColor, activeColor, disabledColor));
+    }
+
+    private static StateListDrawable setupStateDrawable(ViewGroupPosition position, int strokeWidth, GradientDrawable defaultGd, GradientDrawable activeGd, GradientDrawable disabledGd) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        LayerDrawable defaultLayer = new LayerDrawable(new Drawable[]{defaultGd});
+        LayerDrawable activeLayer = new LayerDrawable(new Drawable[]{activeGd});
+        LayerDrawable disabledLayer = new LayerDrawable(new Drawable[]{disabledGd});
+
+        LayerDrawable[] ldAry = new LayerDrawable[]{defaultLayer, activeLayer, disabledLayer};
+        int n = strokeWidth * -1;
+
+        // use LayerDrawable to hide strokes on one side of the drawable (if needed), using negative insets
+        if (position != null) {
+            switch (position) {
+                case MIDDLE_HORI:
+                    setInsetOnLayers(ldAry, n, 0, 0, 0);
+                    break;
+                case END:
+                    setInsetOnLayers(ldAry, n, 0, 0, 0);
+                    break;
+                case MIDDLE_VERT:
+                    setInsetOnLayers(ldAry, 0, n, 0, 0);
+                    break;
+                case BOTTOM:
+                    setInsetOnLayers(ldAry, 0, n, 0, 0);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= 14) {
+            stateListDrawable.addState(new int[]{android.R.attr.state_hovered}, activeLayer);
+        }
+
+        stateListDrawable.addState(new int[]{android.R.attr.state_activated}, activeLayer);
+        stateListDrawable.addState(new int[]{android.R.attr.state_focused}, activeLayer);
+        stateListDrawable.addState(new int[]{android.R.attr.state_pressed}, activeLayer);
+        stateListDrawable.addState(new int[]{android.R.attr.state_selected}, activeLayer);
+        stateListDrawable.addState(new int[]{-android.R.attr.state_enabled}, disabledLayer);
+        stateListDrawable.addState(new int[]{}, defaultLayer);
+
+        return stateListDrawable;
+    }
+
+    private static void setupDrawableCorners(ViewGroupPosition position, boolean rounded, int cornerRadius, GradientDrawable defaultGd, GradientDrawable activeGd, GradientDrawable disabledGd) {
+        if (rounded) {
+            if (position == ViewGroupPosition.SOLO) {
+                defaultGd.setCornerRadius(cornerRadius);
+                activeGd.setCornerRadius(cornerRadius);
+                disabledGd.setCornerRadius(cornerRadius);
+            }
+            else {
+                float[] radii; // X/Y pairs for top-left, top-right, bottom-right, bottom-left.
+                float r = cornerRadius;
+
+                switch (position) {
+                    case MIDDLE_HORI:
+                        radii = new float[]{0, 0, 0, 0, 0, 0, 0, 0};
+                        break;
+                    case MIDDLE_VERT:
+                        radii = new float[]{0, 0, 0, 0, 0, 0, 0, 0};
+                        break;
+                    case TOP:
+                        radii = new float[]{r, r, r, r, 0, 0, 0, 0};
+                        break;
+                    case BOTTOM:
+                        radii = new float[]{0, 0, 0, 0, r, r, r, r};
+                        break;
+                    case START:
+                        radii = new float[]{r, r, 0, 0, 0, 0, r, r,};
+                        break;
+                    case END:
+                        radii = new float[]{0, 0, r, r, r, r, 0, 0};
+                        break;
+                    default:
+                        radii = new float[]{0, 0, 0, 0, 0, 0, 0, 0};
+                        break;
+                }
+
+                defaultGd.setCornerRadii(radii);
+                activeGd.setCornerRadii(radii);
+                disabledGd.setCornerRadii(radii);
+            }
+        }
+    }
+
     private static int[][] getStateList() {
         if (Build.VERSION.SDK_INT >= 14) {
             return new int[][]
@@ -213,84 +309,6 @@ class BootstrapDrawableFactory {
     private static void setInsetOnLayers(LayerDrawable[] ary, int l, int t, int r, int b) {
         for (LayerDrawable ld : ary) {
             ld.setLayerInset(0, l, t, r, b);
-        }
-    }
-
-    private static float[] calculateCornersForPosition(ViewGroupPosition position, float r) {
-        // X/Y pairs for top-left, top-right, bottom-right, bottom-left.
-        switch (position) {
-            case MIDDLE_HORI:
-                return new float[]{0, 0, 0, 0, 0, 0, 0, 0};
-            case MIDDLE_VERT:
-                return new float[]{0, 0, 0, 0, 0, 0, 0, 0};
-            case TOP:
-                return new float[]{r, r, r, r, 0, 0, 0, 0};
-            case BOTTOM:
-                return new float[]{0, 0, 0, 0, r, r, r, r};
-            case START:
-                return new float[]{r, r, 0, 0, 0, 0, r, r,};
-            case END:
-                return new float[]{0, 0, r, r, r, r, 0, 0};
-            default:
-                return new float[]{0, 0, 0, 0, 0, 0, 0, 0};
-        }
-    }
-
-    private static StateListDrawable setupStateDrawable(ViewGroupPosition position, int strokeWidth, GradientDrawable defaultGd, GradientDrawable activeGd, GradientDrawable disabledGd) {
-        StateListDrawable stateListDrawable = new StateListDrawable();
-        LayerDrawable defaultLayer = new LayerDrawable(new Drawable[]{defaultGd});
-        LayerDrawable activeLayer = new LayerDrawable(new Drawable[]{activeGd});
-        LayerDrawable disabledLayer = new LayerDrawable(new Drawable[]{disabledGd});
-
-        LayerDrawable[] ldAry = new LayerDrawable[]{defaultLayer, activeLayer, disabledLayer};
-        int n = strokeWidth * -1;
-
-        // use LayerDrawable to hide strokes on one side of the drawable (if Button is in a group)
-
-        if (position != null) {
-            switch (position) {
-                case MIDDLE_HORI:
-                    setInsetOnLayers(ldAry, n, 0, 0, 0);
-                    break;
-                case END:
-                    setInsetOnLayers(ldAry, n, 0, 0, 0);
-                    break;
-                case MIDDLE_VERT:
-                    setInsetOnLayers(ldAry, 0, n, 0, 0);
-                    break;
-                case BOTTOM:
-                    setInsetOnLayers(ldAry, 0, n, 0, 0);
-            }
-        }
-
-
-        if (Build.VERSION.SDK_INT >= 14) {
-            stateListDrawable.addState(new int[]{android.R.attr.state_hovered}, activeLayer);
-        }
-
-        stateListDrawable.addState(new int[]{android.R.attr.state_activated}, activeLayer);
-        stateListDrawable.addState(new int[]{android.R.attr.state_focused}, activeLayer);
-        stateListDrawable.addState(new int[]{android.R.attr.state_pressed}, activeLayer);
-        stateListDrawable.addState(new int[]{android.R.attr.state_selected}, activeLayer);
-        stateListDrawable.addState(new int[]{-android.R.attr.state_enabled}, disabledLayer);
-        stateListDrawable.addState(new int[]{}, defaultLayer);
-
-        return stateListDrawable;
-    }
-
-    private static void setupDrawableCorners(ViewGroupPosition position, boolean rounded, int cornerRadius, GradientDrawable defaultGd, GradientDrawable activeGd, GradientDrawable disabledGd) {
-        if (rounded) {
-            if (position == ViewGroupPosition.SOLO) {
-                defaultGd.setCornerRadius(cornerRadius);
-                activeGd.setCornerRadius(cornerRadius);
-                disabledGd.setCornerRadius(cornerRadius);
-            }
-            else {
-                float[] radii = calculateCornersForPosition(position, cornerRadius);
-                defaultGd.setCornerRadii(radii);
-                activeGd.setCornerRadii(radii);
-                disabledGd.setCornerRadii(radii);
-            }
         }
     }
 
--- TODO.md
+++ TODO.md
@@ -1,21 +1,3 @@
-Fix/Priority List
-=============
-
-* Test on real device
-* Publish on play store
-
-Code Review
-===========
-
-#Implementation final points:
-Tidy up drawable factory
-
-* Final Lint check
-
-
-
-
-
 Feature (Wish)List
 =============
 * More Views in Bootstrap Spec
